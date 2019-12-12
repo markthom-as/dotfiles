798421c864ff2d41721ab71e3aca0dd789b742e3
Mark Thomas <m.marcantano@gmail.com>
2019-12-12 13:01:22 -0800

    fix zsh setup

--- scripts/oh-my-zsh-setup.sh
+++ scripts/oh-my-zsh-setup.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+if [ ! -f "$HOME/.oh-my-zsh" ]; then
+    echo "Installing oh-my-zsh..."
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
