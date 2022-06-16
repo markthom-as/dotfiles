e311a26c3e7e204eacf904455dd87c1bd35f3ece
Mark Marcantano <m.marcantano@gmail.com>
2022-06-16 13:56:23 -0700

    remove files and start fresh!

--- scripts/oh-my-zsh-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ ! -f "$HOME/.oh-my-zsh" ]; then
-    echo "Installing oh-my-zsh..."
-    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-fi
